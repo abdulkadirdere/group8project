d3d4f63c8aafd642a3cb82098d2fa5ca209e5fae
abdulkadirdere <752817@students.wits.ac.za>
2017-09-16 18:22:25 +0200

    views updated by Jesse

---
+++ development/Group8/Controllers/PGFOsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Group8.DAL;
+using Group8.Models;
+
+namespace Group8.Controllers
+{
+    public class PGFOsController : Controller
+    {
+        private PAASContext db = new PAASContext();
+
+        // GET: PGFOs
+        public ActionResult Index()
+        {
+            return View(db.PGFOs.ToList());
+        }
+
+        // GET: PGFOs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PGFO pGFO = db.PGFOs.Find(id);
+            if (pGFO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pGFO);
+        }
+
+        // GET: PGFOs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PGFOs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PGFO_ID,PGFO_First_Name,PGFO_Last_Name,PGFO_ID_Number,PGFO_Email,PGFO_Contact_No,PGFO_Status")] PGFO pGFO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PGFOs.Add(pGFO);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pGFO);
+        }
+
+        // GET: PGFOs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PGFO pGFO = db.PGFOs.Find(id);
+            if (pGFO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pGFO);
+        }
+
+        // POST: PGFOs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PGFO_ID,PGFO_First_Name,PGFO_Last_Name,PGFO_ID_Number,PGFO_Email,PGFO_Contact_No,PGFO_Status")] PGFO pGFO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pGFO).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pGFO);
+        }
+
+        // GET: PGFOs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PGFO pGFO = db.PGFOs.Find(id);
+            if (pGFO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pGFO);
+        }
+
+        // POST: PGFOs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PGFO pGFO = db.PGFOs.Find(id);
+            db.PGFOs.Remove(pGFO);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ development/Group8/Models/Evaluator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Group8.Models
+{
+    public class Evaluator
+    {
+        [Key]
+        public int Evaluator_ID { get; set; }
+        [Required(ErrorMessage = "Please enter a first name.")]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Display(Name = "First Name*")]
+        public string Evaluator_First_Name { get; set; }
+        public string Evaluator_Last_Name { get; set; }
+        public string Evaluator_ID_Number { get; set; }
+        public string Evaluator_Email { get; set; }
+        public string Evaluator_Contact_No { get; set; }
+        public string Evaluator_Type { get; set; }
+        public string Evaluator_Password { get; set; }
+        public string Evaluator_Status { get; set; }
+
+        public virtual ICollection<Interview> Interviews { get; set; }
+        public virtual ICollection<Application> Application { get; set; }
+    }
+}
